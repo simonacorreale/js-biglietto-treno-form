379def0495f1b41db16f7b0e60cf74894e364319
simonacorreale <simona.correale.work@gmail.com>
2024-12-16 19:36:55 +0100

    fix: button spinner

--- sript.js
+++ sript.js
@@ -10,12 +10,12 @@ console.log (resultMessage);
 submitButton.addEventListener ('click', function (event) {
     event.preventDefault ();
 
-    submitButton.disabled =true;
-    submitButton.querySelector('#spinner').classList.toggle('d-none');
-    const spinnerSelector = submitButton.querySelector('#spinner');
+
     const statusSpan = submitButton.querySelector('.status');
     const originalTextStatus = statusSpan.innerHTML;
-    spinnerSelector.classList.toggle('d-none');
+
+    submitButton.disabled =true;
+    submitButton.querySelector('#spinner').classList.toggle('d-none');
     statusSpan.innerHTML = 'Loading...';
 
 
@@ -41,12 +41,15 @@ submitButton.addEventListener ('click', function (event) {
     console.log(ageInput.value)
     console.log ({originalTextStatus})
     
-    submitButton.disabled =false;
-    spinnerSelector.classList.toggle('d-none')
-    statusSpan.innerHTML = originalTextStatus;
-    
+   
     const finalPrice = totalPrice.toFixed(2);
 
+    submitButton.disabled =false;
+    submitButton.querySelector ('#spinner').classList.toggle('d-none');
+    submitButton.querySelector('.status').innerHTML = originalTextStatus;
+
+   
+    
     // Mostra il risultato nella UI
     resultMessage.innerHTML = `${finalPrice}`;
     return;
