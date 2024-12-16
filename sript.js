872638eb3e19155f5544b9057cbb39b2f9ab675c
simonacorreale <simona.correale.work@gmail.com>
2024-12-16 23:26:41 +0100

    fix: loader works

--- sript.js
+++ sript.js
@@ -1,62 +1,57 @@
-const submitButton = document.querySelector ('#action');
-console.log (submitButton);
+const submitButton = document.querySelector('#action');
+console.log(submitButton);
 const distanceInput = document.querySelector('#distance-km');
 const ageInput = document.querySelector('#user-age');
-const resultMessage = document.querySelector ('#final-results');
+const resultMessage = document.querySelector('#final-results');
+distanceInput.value = 0
+ageInput.value = 0
 
-console.log (resultMessage);
+// inizio funzione click
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
 
-
-submitButton.addEventListener ('click', function (event) {
-    event.preventDefault ();
-
-
+    //  Spinner load
+    const spinnerEl = submitButton.querySelector('#spinner')
     const statusSpan = submitButton.querySelector('.status');
     const originalTextStatus = statusSpan.innerHTML;
-
-    submitButton.disabled =true;
-    submitButton.querySelector('#spinner').classList.toggle('d-none');
-    statusSpan.innerHTML = 'Loading...';
-
-
     const distance = parseInt(distanceInput.value);
     const age = parseInt(ageInput.value);
-  
-    if (isNaN(distance) || isNaN(age) || distance <= 0 || age <= 0) {
-        resultMessage.innerHTML = "nessun prezzo stimato";
-        return;
-        console.log (resultMessage)
-    } 
-    // Calcola il prezzo del biglietto
+
+    // Status base del bottone
+    submitButton.disabled = true;
+    spinnerEl.classList.remove('d-none')
+    statusSpan.innerHTML = 'Loading...'
+
+    // Calcolo percentuale
     const priceKm = 0.21;
     let totalPrice = distance * priceKm;
 
-     if (age < 18) {
+    if (age < 18) {
         totalPrice *= 0.8; // Sconto del 20% per i minorenni
     } else if (age >= 65) {
         totalPrice *= 0.6; // Sconto del 40% per gli anziani
-    } 
+    }
 
     console.log(distanceInput.value)
     console.log(ageInput.value)
-    console.log ({originalTextStatus})
-    
-   
+    console.log({ originalTextStatus })
+
     const finalPrice = totalPrice.toFixed(2);
 
-    submitButton.disabled =false;
-    submitButton.querySelector ('#spinner').classList.toggle('d-none');
-    submitButton.querySelector('.status').innerHTML = originalTextStatus;
+    setTimeout(() => {
 
-   
-    
-    // Mostra il risultato nella UI
-    resultMessage.innerHTML = `${finalPrice}`;
-    return;
+        spinnerEl.classList.add('d-none')
+        statusSpan.innerHTML = originalTextStatus
+        submitButton.disabled = false;
+        resultMessage.innerHTML = `${finalPrice}`;
 
+        if (isNaN(finalPrice)) {
+            resultMessage.innerHTML = "nessun prezzo stimato";
+        }
+        console.log("loaderTimer")
+    }, "1000");
 
-    
 })
 
-   
+
 
